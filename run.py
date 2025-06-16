e253eb1e8d10432ac919719b0db1f82798e07553
Shiva Gupta <shivaiitpatna@gmail.com>
2025-06-17 02:51:11 +0530

    Fixes the background as dark

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/login')
+def login_page():
     return render_template('login.html')
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
